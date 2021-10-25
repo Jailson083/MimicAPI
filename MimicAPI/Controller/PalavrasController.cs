814c0282b64fdc8939342ebaf29679029111cf64
Jailson Evangelista <jailson.teixeira@kobel.com.br>
2021-10-24 23:17:32 -0300

    Criação das Controllers

---
+++ MimicAPI/Controller/PalavrasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MimicAPI.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicAPI.Controller
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class PalavrasController : ControllerBase
+    {
+        
+        [HttpGet]
+        [Route("getAll")]
+
+        public async Task<IActionResult> Get([FromServices]MimicContext context)
+        {
+            var mimic = await context.Palavras
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(mimic);
+
+        }
+
+    }
+}
